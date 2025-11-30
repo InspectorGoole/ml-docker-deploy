0caea7297fc4e12102928ec8795431e978650d19
InspectorGoole <am6499057@gmail.com>
2025-12-01 02:48:02 +0400

    filename error

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     response = app.test_client().get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello world"
+    assert response.data == b"Hello world" # this should be like this bcuz we get an error in github actions, cuz flask expects the output in bytes
